a6e5e14095771d17f311008155e6b364eab11045
吴焯斌 <920658022@qq.com>
2022-09-08 16:33:02 +0800

    feat:fix

--- src/es/promise/index.js
+++ src/es/promise/index.js
@@ -3,58 +3,67 @@ import { isTypeOf } from '@/utils/tools';
 /**
  *  Promise对象解析
  *  @Prototype 自己
- *  @PromiseState 三种，resolved、rejected、pending。
- *  @PromiseResult pending为undefined，resolved和rejected时跟随传入的参数。
+ *  @PromiseState 三种，fulfilled、rejected、pending。
+ *  @PromiseResult pending为undefined，fulfilled和rejected时跟随传入的参数。
+ *  @then .then() 方法需要两个参数，第一个参数作为处理已兑现状态（fulfilled）的回调函数，而第二个参数则作为处理已拒绝状态的回调函数。
+ *        每一个 .then() 方法还会返回一个新生成的 promise 对象，这个对象可被用作链式调用。
  *
  *  new Promise过程解析
- *  @resolver Promise实例必须添加一个resolver回调函数作为参数
+ *  @resolver Promise实例必须添加一个resolver回调函数作为参数，resolver需包含resolve和reject两个参数
+ *
  */
 function MyPromise(resolver) {
   const that = this;
   this.PromiseState = 'pending';
   this.PromiseResult = undefined;
-  this.onResolveCallBack = [];
-  this.onRejectCallBack = [];
+  this.onFulfilledCallBack = [];
+  this.onRejectedCallBack = [];
   try {
     if (!isTypeOf(resolver, 'function')) {
       throw new Error('请添加回调函数');
     }
     resolver(resolve, reject);
   } catch (error) {
-    console.log(error);
-    console.log(that.catch);
     reject(error);
   }
   function resolve(value) {
     if (that.PromiseState === 'pending') {
-      that.PromiseState = 'resolved';
+      that.PromiseState = 'fulfilled';
       that.PromiseResult = value;
-      that.onResolveCallBack.forEach((cb) => cb(value));
+      that.onFulfilledCallBack.forEach((cb) => cb(value));
     }
   }
   function reject(reason) {
     if (that.PromiseState === 'pending') {
       that.PromiseState = 'rejected';
       that.PromiseResult = reason;
-      that.onRejectCallBack.forEach((cb) => cb(reason));
+      that.onRejectedCallBack.forEach((cb) => cb(reason));
     }
   }
 }
 
-MyPromise.prototype.then = function then(callBack) {
-  if (!isTypeOf(callBack, 'function') || !isTypeOf(this.onResolveCallBack, 'array')) return;
-  this.onResolveCallBack.push(callBack);
-  return this;
-};
+MyPromise.prototype.then = function then(onFulfilled, onRejected) {
+  if (!isTypeOf(onFulfilled, 'function')) {
+    onFulfilled = (value) => value;
+  }
+  if (!isTypeOf(onRejected, 'function')) {
+    onRejected = (reason) => reason;
+  }
 
-MyPromise.prototype.catch = function myCatch(callBack) {
-  if (!isTypeOf(callBack, 'function') || !isTypeOf(this.onRejectCallBack, 'array')) return;
-  this.onRejectCallBack.push(callBack);
-  return this;
-};
-
-MyPromise.prototype.finally = function myFinally() {
-  console.log('finally');
+  const newPromise = new MyPromise((resolve, reject) => {
+    console.log(this.PromiseState);
+    if (this.PromiseState === 'pending') {
+      this.onFulfilledCallBack.push(onFulfilled);
+      this.onRejectedCallBack.push(onRejected);
+    }
+    if (this.PromiseState === 'fulfilled') {
+      onFulfilled(this.PromiseResult);
+    }
+    if (this.PromiseState === 'rejected') {
+      onRejected(this.PromiseResult);
+    }
+  });
+  return newPromise;
 };
 
 console.log(MyPromise);
@@ -62,35 +71,29 @@ console.log(MyPromise);
 const promise1 = new MyPromise((resolve, reject) => {
   setTimeout(() => {
     resolve('promise------1');
-  }, 1000);
+  }, 2000);
 });
 
 const promise2 = new MyPromise((resolve, reject) => {
   // console.log(that.add());
-  resolve('promise------2');
-  // setTimeout(() => {
 
-  // }, 2000);
+  setTimeout(() => {
+    resolve('promise------2');
+  }, 2000);
 });
 
-console.log('promise2', promise2);
+promise1.then(
+  (res) => {
+    console.log('res1', res);
+    return promise2;
+  },
+  (err) => {
+    console.log('err1', err);
+  },
+);
 
-const then1 = promise1
-  .then((res) => {
-    console.log('then1 result', res);
-    return 'then1';
-  })
-  .catch((err) => {
-    console.log('then1 error', err);
-  });
+promise1.then((res) => {
+  console.log('res2', res);
+});
 
-const then2 = promise2
-  .then((res) => {
-    console.log('then2 result', res);
-    return 'then2';
-  })
-  .catch((err) => {
-    console.log('then2 error', err);
-  });
-
-console.log(then1, then2);
+console.log('promise1', promise1);
