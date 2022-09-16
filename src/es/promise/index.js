6756cfa01ee7cc1fb2017d44db09274e134d8c93
吴焯斌 <920658022@qq.com>
2022-09-16 18:07:02 +0800

    feat:fix

--- src/es/promise/index.js
+++ src/es/promise/index.js
@@ -12,9 +12,15 @@ import { isTypeOf } from '@/utils/tools';
  *  @resolver Promise实例必须添加一个resolver回调函数作为参数，resolver需包含resolve和reject两个参数
  *
  */
+
+const EPromiseState = {
+  Pending: 'pending',
+  Fulfilled: 'fulfilled',
+  Rejected: 'rejected',
+};
 function MyPromise(resolver) {
   const that = this;
-  this.PromiseState = 'pending';
+  this.PromiseState = EPromiseState.Pending;
   this.PromiseResult = undefined;
   this.onFulfilledCallBack = [];
   this.onRejectedCallBack = [];
@@ -27,15 +33,18 @@ function MyPromise(resolver) {
     reject(error);
   }
   function resolve(value) {
-    if (that.PromiseState === 'pending') {
-      that.PromiseState = 'fulfilled';
+    if (that.PromiseState === EPromiseState.Pending) {
+      that.PromiseState = EPromiseState.Fulfilled;
       that.PromiseResult = value;
+      // 因为一个Promise可以有多个then函数，可以有多个地方调用。
+      // 为了多个then函数内部之间互不干扰，做类似发布订阅的操作，把then的回调函数放在数组里，
+      // 确保每个then里的回调函数都能被调用。
       that.onFulfilledCallBack.forEach((cb) => cb(value));
     }
   }
   function reject(reason) {
-    if (that.PromiseState === 'pending') {
-      that.PromiseState = 'rejected';
+    if (that.PromiseState === EPromiseState.Pending) {
+      that.PromiseState = EPromiseState.Fulfilled;
       that.PromiseResult = reason;
       that.onRejectedCallBack.forEach((cb) => cb(reason));
     }
@@ -49,23 +58,36 @@ MyPromise.prototype.then = function then(onFulfilled, onRejected) {
   if (!isTypeOf(onRejected, 'function')) {
     onRejected = (reason) => reason;
   }
-
+  console.log(onFulfilled);
   const newPromise = new MyPromise((resolve, reject) => {
-    if (this.PromiseState === 'pending') {
+    console.log(this.PromiseState);
+    if (this.PromiseState === EPromiseState.Pending) {
       this.onFulfilledCallBack.push(onFulfilled);
       this.onRejectedCallBack.push(onRejected);
     }
-    if (this.PromiseState === 'fulfilled') {
-      onFulfilled(this.PromiseResult);
+    if (this.PromiseState === EPromiseState.Fulfilled) {
+      const x = onFulfilled(this.PromiseResult);
+      resolvePromise(newPromise, x, resolve, reject);
     }
-    if (this.PromiseState === 'rejected') {
+    if (this.PromiseState === EPromiseState.Rejected) {
       onRejected(this.PromiseResult);
     }
   });
   return newPromise;
 };
 
-console.log(MyPromise);
+function resolvePromise(newPromise, x, resolve, reject) {
+  if (newPromise === x) {
+    throw new Error('不能返回自身');
+  }
+  // 判断x是不是 MyPromise 实例对象
+  console.log(x);
+  if (x instanceof MyPromise) {
+    x.then(resolve, reject);
+  } else {
+    resolve(x);
+  }
+}
 
 const promise1 = new MyPromise((resolve, reject) => {
   setTimeout(() => {
@@ -84,22 +106,29 @@ const promise2 = new MyPromise((resolve, reject) => {
   }
 });
 
-promise1.then(
-  (res) => {
-    console.log('res1', res);
-  },
-  (err) => {
-    console.log('err1', err);
-  },
-);
+const promise3 = () =>
+  new MyPromise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('promise------3');
+    }, 1000);
+  });
 
-promise2.then(
-  (res) => {
-    console.log('res2', res);
-  },
-  (err) => {
-    console.log('err2', err);
-  },
-);
+promise1
+  .then((res) => {
+    console.log('res1', res);
+    return promise3();
+  })
+  .then((res) => {
+    console.log('res1_1', res);
+  });
+
+// promise2.then(
+//   (res) => {
+//     console.log('res2', res);
+//   },
+//   (err) => {
+//     console.log('err2', err);
+//   },
+// );
 
 console.log('promise1', promise1);
